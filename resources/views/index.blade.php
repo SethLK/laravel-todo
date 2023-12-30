a23e748eef1e77b29c690f2ac0e8839fa5a2b170
SethLK <sethlk2006@gmail.com>
2023-12-30 15:54:46 +0630

    Little CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,58 +1,62 @@
 <!-- resources/views/index.blade.php -->
 
 @include('components.head')
-
-<div class="add_todo">
-    <form action="{{ route("store") }}" method="post">
-        @csrf
-        @method("POST")
-        <label for="Tasks">Tasks
-            @error('name')
-                <input type="text" name="name" placeholder="Task Name" value="{{ old("name") }}">
-                <span class="error">{{ $message }}</span>
-            @else
-                <input type="text" name="name" placeholder="Task Name">
-            @enderror
-        </label>
-        <button type="submit">Add</button>
-    </form>
+<div class="center">
+    <div class="container">
+        
+        <div class="add_todo">
+            <form action="{{ route("store") }}" method="post">
+                @csrf
+                @method("POST")
+                <label for="Tasks">Tasks
+                    @error('name')
+                        <input type="text" name="name" placeholder="Task Name" value="{{ old("name") }}">
+                        <span class="error">{{ $message }}</span>
+                    @else
+                        <input type="text" name="name" placeholder="Task Name">
+                    @enderror
+                </label>
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    
+        @if(session('success'))
+            <div class="success-message">{{ session('success') }}</div>
+        @endif
+    
+        <div class="tasks">
+            <ul>
+                @foreach ($tasks as $task)
+                    <li>
+                        @if ($task->completed == 0)
+                            <a href="/edit/{{ $task->id }}">{{ $task->name }}</a>
+                            <form action="{{ route('complete', ['id' => $task->id]) }}" method="POST" style="display: inline;">
+                                @csrf
+                                @method("PUT")
+                                <button type="submit">Done</button>
+                            </form>
+                            <form action="{{ route("delete", ['id' => $task->id]) }}" method="POST" style="display: inline;">
+                                @csrf
+                                @method("DELETE")
+                                <button type="submit">Delete</button>
+                            </form>
+                        @else
+                            <s>{{ $task->name }}</s>
+                            <form action="{{ route("delete", ['id' => $task->id]) }}" method="POST" style="display: inline;">
+                                @csrf
+                                @method("DELETE")
+                                <button type="submit">Delete</button>
+                            </form>           
+                        @endif
+                        
+                        
+                        <!-- Use a button or link to trigger the form submission -->
+                        
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    
+    </div>
 </div>
-
-@if(session('success'))
-    <div class="success-message">{{ session('success') }}</div>
-@endif
-
-<div class="tasks">
-    <ul>
-        @foreach ($tasks as $task)
-            <li>
-                @if ($task->completed == 0)
-                    <a href="/edit/{{ $task->id }}">{{ $task->name }}</a>
-                    <form action="{{ route('complete', ['id' => $task->id]) }}" method="POST" style="display: inline;">
-                        @csrf
-                        @method("PUT")
-                        <button type="submit">Done</button>
-                    </form>
-                    <form action="{{ route("delete", ['id' => $task->id]) }}" method="POST" style="display: inline;">
-                        @csrf
-                        @method("DELETE")
-                        <button type="submit">Delete</button>
-                    </form>
-                @else
-                    <s>{{ $task->name }}</s>
-                    <form action="{{ route("delete", ['id' => $task->id]) }}" method="POST" style="display: inline;">
-                        @csrf
-                        @method("DELETE")
-                        <button type="submit">Delete</button>
-                    </form>           
-                @endif
-                
-                
-                <!-- Use a button or link to trigger the form submission -->
-                
-            </li>
-        @endforeach
-    </ul>
-</div>
-
 @include('components.footer')
